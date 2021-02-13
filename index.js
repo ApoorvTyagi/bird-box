d46dfff233216fb109420e84bb510fb6b952e0ca
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2021-02-13 19:18:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function updateGist(tweet) {
           filename: `@${twitterHandle} - ${timeAgo} ago | ❤ ${
             tweet.favorite_count
           } | 🔁 ${tweet.retweet_count}`,
-          content: wrapAnsi(tweet.text, 62, { hard: true })
+          content: wrapAnsi(tweet.text, 59, { hard: true })
         }
       }
     });
